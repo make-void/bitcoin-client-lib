a2d9941cc01c4d2a3cbdc48273b197e62df04d18
Francesco (makevoid) <makevoid@gmail.com>
2015-04-05 04:45:06 +0100

    add transproc, make an irb api

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem "bitcoin-client", github: "sinisterchipmunk/bitcoin-client", require: "bitcoin-client"
+gem "transproc", github: "solnic/transproc", require: "transproc/all"
--- bitcoin_client_test.rb
+++ bitcoin_client_test.rb
@@ -10,8 +10,51 @@ def btc_rpc_password(path)
   pass.strip
 end
 
+
+
+
 PASSWORD = btc_rpc_password "~/.bitcoin/bitcoin.conf"
 
 BC = BitcoinClient::Client.new 'bitcoin', PASSWORD
 
 puts BC.balance
+
+
+# define some console helpers
+
+def reload
+  load File.expand_path __FILE__
+end
+
+def balance
+  BC.balance
+end
+
+def address
+  BC.getnewaddress
+end
+
+def address_balance(addr)
+  BC.balance addr
+end
+
+def keypair
+  BC.get
+end
+
+def private_key
+  BC.dumpprivkey
+end
+
+def send_btc
+  # BC....
+end
+
+alias :transfer :send_btc
+
+def info
+  puts BC.help
+  puts
+  puts "finished printing `bitcoin-cli help`-like command sent via json"
+  true
+end
